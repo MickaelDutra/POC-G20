99d9da2ae540db3a3a34cc1277fb247e19aa6f58
mickael_dutra <mickael.santos@cwi.com.br>
2026-03-26 15:50:03 -0300

    add: commit inicial

---
+++ api/src/main/java/br/com/cwi/intellifinance/IntellifinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cwi.intellifinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellifinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellifinanceApplication.class, args);
+	}
+
+}
