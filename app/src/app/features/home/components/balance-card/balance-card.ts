2a440ad8f6a17740690242d5a451f0e4cefe7075
tassoni <lucas.januario@cwi.com.br>
2026-03-26 22:10:49 -0300

    feat: card de saldo do usuario

---
+++ app/src/app/features/home/components/balance-card/balance-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-card',
+  templateUrl: './balance-card.html',
+  styleUrl: './balance-card.css',
+  standalone: true,
+})
+export class BalanceCard {}
