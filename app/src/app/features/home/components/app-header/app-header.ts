68f3ee3e3b9c097570fedb538e693da59b383ce5
tassoni <lucas.januario@cwi.com.br>
2026-03-26 22:11:43 -0300

    feat: header da tela home

---
+++ app/src/app/features/home/components/app-header/app-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './app-header.html',
+  styleUrl: './app-header.css',
+  standalone: true,
+})
+export class AppHeader {}
